98d1f130325ae6b85c41b7579b27ebaa64417a28
user <user@debian>
2022-07-12 18:59:29 +0300

    built with walletconnect, is working on android with metamask installed

--- lib/blockchain/walletconnect.dart
+++ lib/blockchain/walletconnect.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
+import 'package:walletconnect_secure_storage/walletconnect_secure_storage.dart';
 
 var sessionStatus;
 var address;
 var chainId;
 Future<void> connectWallet() async {
+  WalletConnectSecureStorage sessionStorage = WalletConnectSecureStorage();
+  final session = await sessionStorage.getSession();
+
   final connector = WalletConnect(
     bridge: 'https://bridge.walletconnect.org',
+    session: session,
+    sessionStorage: sessionStorage,
     clientMeta: const PeerMeta(
       name: 'WalletConnect',
       description: 'WalletConnect Developer App',
@@ -37,13 +44,29 @@ Future<void> connectWallet() async {
     debugPrint("disconnect: " + session.toString());
   });
 
+  _launchURL(url) async {
+    // const url = 'https://flutter.io';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      await launch(url);
+      throw 'Could not launch $url';
+    }
+  }
+
   // Create a new session
   if (!connector.connected) {
     sessionStatus = await connector.createSession(
       chainId: 137, //pass the chain id of a network. 137 is Polygon
       onDisplayUri: (uri) {
+        print(uri);
         //AppMehtods.openUrl(uri); //call the launchUrl(uri) method
+        // _launchURL(uri);
+        launch(uri);
       },
+      // onDisplayUri: (uri) => print(uri),
     );
+    debugPrint("starting session");
   }
 }
+// launchUrl(Uri.parse(uri));
