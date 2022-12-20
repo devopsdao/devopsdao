b9618c5e4cab89646eef363f3347bbaa317fa74a
vasya <vasya@soft.dev>
2022-12-20 17:31:09 +0200

    connect wallet button

--- lib/widgets/tags/tags.dart
+++ lib/widgets/tags/tags.dart
@@ -1,4 +1,5 @@
 import 'package:another_flushbar/flushbar.dart';
+import 'package:filter_list/filter_list.dart';
 import 'package:nanoid/nanoid.dart';
 import 'package:provider/provider.dart';
 import 'package:throttling/throttling.dart';
@@ -40,7 +41,27 @@ class _TagsPageState extends State<TagsPage> {
               final double keyboardSize = MediaQuery.of(context).viewInsets.bottom;
               final double screenHeightSizeNoKeyboard = constraints.maxHeight - 70;
               final double screenHeightSize = screenHeightSizeNoKeyboard - keyboardSize;
-              return Center();
+              return FilterListWidget<User>(
+                listData: userList,
+                selectedListData: selectedUserList,
+                onApplyButtonClick: (list) {
+                  // do something with list ..
+                },
+                choiceChipLabel: (item) {
+                  /// Used to display text on chip
+                  return item!.name;
+                },
+                validateSelectedItem: (list, val) {
+                  ///  identify if item is selected or not
+                  return list!.contains(val);
+                },
+                onItemSearch: (user, query) {
+                  /// When search query change in search bar then this method will be called
+                  ///
+                  /// Check if items contains query
+                  return user.name!.toLowerCase().contains(query.toLowerCase());
+                },
+              ),
             }),
           );
         }),
@@ -48,4 +69,58 @@ class _TagsPageState extends State<TagsPage> {
     );
   }
 }
+
+class SimpleTags {
+  final String? tag;
+  final String? avatar;
+  SimpleTags({this.tag, this.avatar});
+}
+
+List<SimpleTags> SimpleTagsList = [
+  SimpleTags(tag: "C++", avatar: ""),
+  SimpleTags(tag: "Javascript ", avatar: ""),
+  SimpleTags(tag: "Valarie ", avatar: ""),
+  SimpleTags(tag: "Elyse ", avatar: ""),
+  SimpleTags(tag: "Ethel ", avatar: ""),
+  SimpleTags(tag: "Emelyan ", avatar: ""),
+  SimpleTags(tag: "Catherine ", avatar: ""),
+  SimpleTags(tag: "Stepanida  ", avatar: ""),
+  SimpleTags(tag: "Carolina ", avatar: ""),
+  SimpleTags(tag: "Nail  ", avatar: ""),
+  SimpleTags(tag: "Kamil ", avatar: ""),
+  SimpleTags(tag: "Mariana ", avatar: ""),
+  SimpleTags(tag: "Katerina ", avatar: ""),
+];
+
+// class FilterPage extends StatelessWidget {
+//   const FilterPage({Key? key, this.selectedUserList})
+//       : super(key: key);
+//   final List<User>? selectedUserList;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: FilterListWidget<User>(
+//         listData: userList,
+//         selectedListData: selectedUserList,
+//         onApplyButtonClick: (list) {
+//           // do something with list ..
+//         },
+//         choiceChipLabel: (item) {
+//           /// Used to display text on chip
+//           return item!.name;
+//         },
+//         validateSelectedItem: (list, val) {
+//           ///  identify if item is selected or not
+//           return list!.contains(val);
+//         },
+//         onItemSearch: (user, query) {
+//           /// When search query change in search bar then this method will be called
+//           ///
+//           /// Check if items contains query
+//           return user.name!.toLowerCase().contains(query.toLowerCase());
+//         },
+//       ),
+//     );
+//   }
+// }
 
