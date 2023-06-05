973930a05ecc9afc85ef2a8fd29f43e2a696d319
user1 <user1@devopsdao>
2023-06-05 14:48:43 +0300

    added rive splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:dodao/widgets/tags/search_services.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:rive_splash_screen/rive_splash_screen.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -58,9 +59,9 @@ void main() async {
         //     return searchServices!..nftBalanceMap = tasksServices.resultNftsMap;
         //   },
         // ),
-        ChangeNotifierProxyProvider<TasksServices , SearchServices>(
+        ChangeNotifierProxyProvider<TasksServices, SearchServices>(
           create: (_) => SearchServices(),
-          update: (_,tasksServices , searchServices) {
+          update: (_, tasksServices, searchServices) {
             searchServices!.nftCollectionMap = tasksServices.resultInitialCollectionMap;
             return searchServices..nftBalanceMap = tasksServices.resultNftsMap;
           },
@@ -121,153 +122,146 @@ class _MyAppState extends State<MyApp> {
       DeviceOrientation.portraitDown,
     ]);
     return ChangeNotifierProvider(
-      create: (_) => ModelTheme(),
-      child: Consumer<ModelTheme>(
-      builder: (context, ModelTheme themeNotifier, child) {
-        return MaterialApp.router(
-          routerDelegate: beamerDelegate,
-          routeInformationParser: BeamerParser(),
-          backButtonDispatcher: BeamerBackButtonDispatcher(delegate: beamerDelegate),
-          debugShowCheckedModeBanner: false,
-          title: 'dodao.dev',
-          localizationsDelegates: const [
-            FFLocalizationsDelegate(),
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          locale: _locale,
-          supportedLocales: const [Locale('en', '')],
-          theme:
+        create: (_) => ModelTheme(),
+        child: Consumer<ModelTheme>(builder: (context, ModelTheme themeNotifier, child) {
+          return SplashScreen.navigate(
+            name: 'assets/rive_animations/cat-blinking.riv',
+            until: () => Future.delayed(const Duration(seconds: 5)),
+            startAnimation: '1',
+            next: (context) => MaterialApp.router(
+              routerDelegate: beamerDelegate,
+              routeInformationParser: BeamerParser(),
+              backButtonDispatcher: BeamerBackButtonDispatcher(delegate: beamerDelegate),
+              debugShowCheckedModeBanner: false,
+              title: 'dodao.dev',
+              localizationsDelegates: const [
+                FFLocalizationsDelegate(),
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              locale: _locale,
+              supportedLocales: const [Locale('en', '')],
+              theme:
+                  // ******************* Light
+                  ThemeData(
+                fontFamily: GoogleFonts.inter().fontFamily,
+                brightness: Brightness.light,
+                scaffoldBackgroundColor: Colors.white,
+                splashFactory: NoSplash.splashFactory,
+                useMaterial3: true,
 
-          // ******************* Light
-          ThemeData(
-            fontFamily: GoogleFonts.inter().fontFamily,
-            brightness: Brightness.light,
-            scaffoldBackgroundColor: Colors.white,
-            splashFactory: NoSplash.splashFactory,
-            useMaterial3: true,
+                appBarTheme: const AppBarTheme(
+                    // backgroundColor: Colors.black,
+                    color: Colors.white,
+                    surfaceTintColor: Colors.white),
 
-            appBarTheme: const AppBarTheme(
-              // backgroundColor: Colors.black,
-                color: Colors.white,
-                surfaceTintColor: Colors.white
-            ),
-
-            // iconTheme: const IconThemeData(
-            //   color: Colors.black,
-            // ),
-            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              selectedItemColor: Colors.black
-            ),
-
-            // primaryColor: const Color(0xff31d493),
-            // cardColor: Colors.black,
-            textTheme: TextTheme(
-              bodyLarge: TextStyle(fontSize: 20, color: DodaoTheme.of(context).secondaryText),
-              bodyMedium: TextStyle(fontSize: 15, color: DodaoTheme.of(context).secondaryText),
-              bodySmall: TextStyle(fontSize: 12, color: DodaoTheme.of(context).secondaryText),
-              titleLarge: const TextStyle(fontSize: 22, color: Colors.black),
-              titleMedium: const TextStyle(fontSize: 20, color: Colors.black)
-            ),
-
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(155, 40),
-                disabledBackgroundColor: Colors.grey.shade300,
-                backgroundColor: DodaoTheme.of(context).buttonBackgroundColor,
-              ),
-            ),
-          ),
-
-          // ******************* Dark
-
-          darkTheme: ThemeData(
-
-            fontFamily: GoogleFonts.inter().fontFamily,
-            brightness: Brightness.dark,
-            scaffoldBackgroundColor: Colors.black,
-            splashFactory: NoSplash.splashFactory,
-            useMaterial3: true,
-
-            appBarTheme: const AppBarTheme(
-                // systemOverlayStyle: SystemUiOverlayStyle(
-                //     statusBarColor: Colors.white,
-                //     statusBarBrightness: Brightness.light,
-                //     systemNavigationBarColor: Colors.white,
+                // iconTheme: const IconThemeData(
+                //   color: Colors.black,
                 // ),
-              // backgroundColor: Colors.black,
-                color: Colors.black,
-                surfaceTintColor: Colors.black,
-                // toolbarTextStyle: TextStyle(fontSize: 23, color: Colors.white),
-                // titleTextStyle: TextStyle(fontSize: 23, color: Colors.white),
+                bottomNavigationBarTheme: const BottomNavigationBarThemeData(selectedItemColor: Colors.black),
 
-            ),
-            inputDecorationTheme: const InputDecorationTheme(
+                // primaryColor: const Color(0xff31d493),
+                // cardColor: Colors.black,
+                textTheme: TextTheme(
+                    bodyLarge: TextStyle(fontSize: 20, color: DodaoTheme.of(context).secondaryText),
+                    bodyMedium: TextStyle(fontSize: 15, color: DodaoTheme.of(context).secondaryText),
+                    bodySmall: TextStyle(fontSize: 12, color: DodaoTheme.of(context).secondaryText),
+                    titleLarge: const TextStyle(fontSize: 22, color: Colors.black),
+                    titleMedium: const TextStyle(fontSize: 20, color: Colors.black)),
 
-              // fillColor: Colors.orange,
-              // labelStyle: TextStyle(fontSize: 23, color: Colors.white),
-              // floatingLabelStyle: TextStyle(fontSize: 23, color: Colors.white),
-              // helperStyle: TextStyle(fontSize: 23, color: Colors.white),
-              // filled: true,
-            ),
-            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                selectedItemColor: Colors.white70
-            ),
-            // primaryColor: Colors.green,
-            // primarySwatch: Colors.green,
-
-            textTheme: const TextTheme(
-                bodyLarge: TextStyle(fontSize: 19, color: Colors.white70),
-                bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
-                bodySmall: TextStyle(fontSize: 12, color: Colors.white70),
-                titleLarge: TextStyle(fontSize: 22, color: Colors.white),
-                titleMedium: TextStyle(fontSize: 20, color: Colors.white),
-
-            ),
-            textSelectionTheme: const TextSelectionThemeData(
-              cursorColor: Colors.white70,
-              selectionColor: Colors.white70,
-              selectionHandleColor: Colors.white70,
-            ),
-
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(155, 40),
-                // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                disabledBackgroundColor: Colors.grey.shade800,
-                backgroundColor: DodaoTheme.of(context).buttonBackgroundColor,
-                // textStyle: const TextStyle(
-                //     color: Colors.red,
-                //     fontWeight: FontWeight.w400
-                // )
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(155, 40),
+                    disabledBackgroundColor: Colors.grey.shade300,
+                    backgroundColor: DodaoTheme.of(context).buttonBackgroundColor,
+                  ),
+                ),
               ),
-            ),
 
-            // iconTheme: const IconThemeData(
-            //   color: Colors.white,
-            // ),
-          ),
-          // Theme mode settings:
-          themeMode: themeNotifier.isDark  ? ThemeMode.dark : ThemeMode.light,
-          // home: displaySplashImage
-          //     ? Container(
-          //         color: Colors.black,
-          //         child: Center(
-          //           child: Builder(
-          //             builder: (context) => Image.asset(
-          //               'assets/images/logo.png',
-          //               width: MediaQuery.of(context).size.width * 0.6,
-          //               height: MediaQuery.of(context).size.height * 0.6,
-          //               fit: BoxFit.fitWidth,
-          //             ),
-          //           ),
-          //         ),
-          //       )
-          //     : NavBarPage(),
-        );
-      }
-    ));
+              // ******************* Dark
+
+              darkTheme: ThemeData(
+                fontFamily: GoogleFonts.inter().fontFamily,
+                brightness: Brightness.dark,
+                scaffoldBackgroundColor: Colors.black,
+                splashFactory: NoSplash.splashFactory,
+                useMaterial3: true,
+
+                appBarTheme: const AppBarTheme(
+                  // systemOverlayStyle: SystemUiOverlayStyle(
+                  //     statusBarColor: Colors.white,
+                  //     statusBarBrightness: Brightness.light,
+                  //     systemNavigationBarColor: Colors.white,
+                  // ),
+                  // backgroundColor: Colors.black,
+                  color: Colors.black,
+                  surfaceTintColor: Colors.black,
+                  // toolbarTextStyle: TextStyle(fontSize: 23, color: Colors.white),
+                  // titleTextStyle: TextStyle(fontSize: 23, color: Colors.white),
+                ),
+                inputDecorationTheme: const InputDecorationTheme(
+
+                    // fillColor: Colors.orange,
+                    // labelStyle: TextStyle(fontSize: 23, color: Colors.white),
+                    // floatingLabelStyle: TextStyle(fontSize: 23, color: Colors.white),
+                    // helperStyle: TextStyle(fontSize: 23, color: Colors.white),
+                    // filled: true,
+                    ),
+                bottomNavigationBarTheme: const BottomNavigationBarThemeData(selectedItemColor: Colors.white70),
+                // primaryColor: Colors.green,
+                // primarySwatch: Colors.green,
+
+                textTheme: const TextTheme(
+                  bodyLarge: TextStyle(fontSize: 19, color: Colors.white70),
+                  bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
+                  bodySmall: TextStyle(fontSize: 12, color: Colors.white70),
+                  titleLarge: TextStyle(fontSize: 22, color: Colors.white),
+                  titleMedium: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                textSelectionTheme: const TextSelectionThemeData(
+                  cursorColor: Colors.white70,
+                  selectionColor: Colors.white70,
+                  selectionHandleColor: Colors.white70,
+                ),
+
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(155, 40),
+                    // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                    disabledBackgroundColor: Colors.grey.shade800,
+                    backgroundColor: DodaoTheme.of(context).buttonBackgroundColor,
+                    // textStyle: const TextStyle(
+                    //     color: Colors.red,
+                    //     fontWeight: FontWeight.w400
+                    // )
+                  ),
+                ),
+
+                // iconTheme: const IconThemeData(
+                //   color: Colors.white,
+                // ),
+              ),
+              // Theme mode settings:
+              themeMode: themeNotifier.isDark ? ThemeMode.dark : ThemeMode.light,
+              // home: displaySplashImage
+              //     ? Container(
+              //         color: Colors.black,
+              //         child: Center(
+              //           child: Builder(
+              //             builder: (context) => Image.asset(
+              //               'assets/images/logo.png',
+              //               width: MediaQuery.of(context).size.width * 0.6,
+              //               height: MediaQuery.of(context).size.height * 0.6,
+              //               fit: BoxFit.fitWidth,
+              //             ),
+              //           ),
+              //         ),
+              //       )
+              //     : NavBarPage(),
+            ),
+          );
+        }));
   }
 }
 
