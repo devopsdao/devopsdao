740a1be91a21867356aedb53d5f8bc35a74a330c
user1 <user1@devopsdao>
2023-10-11 20:05:06 +0300

    multiple chains support for walletconnect v2

--- lib/blockchain/task_services.dart
+++ lib/blockchain/task_services.dart
@@ -176,7 +176,7 @@ class GetTaskException implements Exception {
 
 class TasksServices extends ChangeNotifier {
   bool hardhatDebug = false;
-  bool hardhatLive = true;
+  bool hardhatLive = false;
   int liveAccount = 0; // choose hardhat account(wallet) to use;
   Map<EthereumAddress, Task> tasks = {};
   Map<EthereumAddress, Task> filterResults = {};
@@ -564,10 +564,10 @@ class TasksServices extends ChangeNotifier {
               session: sessionConnect.session,
             );
             chainId = int.parse(NamespaceUtils.getChainFromAccount(
-              sessionConnect.session.namespaces.values.first.accounts.first,
+              sessionConnect.session.namespaces.values.first.accounts.last,
             ).split(":").last);
             publicAddressWC = EthereumAddress.fromHex(NamespaceUtils.getAccount(
-              sessionConnect.session.namespaces.values.first.accounts.first,
+              sessionConnect.session.namespaces.values.first.accounts.last,
             ));
             publicAddress = publicAddressWC;
 
@@ -652,7 +652,7 @@ class TasksServices extends ChangeNotifier {
         notifyListeners();
       });
 
-      var connectResponse = await walletConnectClient?.createSession();
+      var connectResponse = await walletConnectClient?.createSession(chainId);
 
       final Uri? uri = connectResponse.uri;
 
